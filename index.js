53f9b6e34b2faf08e4b1e5706cbf811bd11db8e2
Paulina Bastista <uwupaulina09@gmail.com>
2024-01-29 18:45:03 -0700

    Funciona el post

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-const app = require("express");
-const server = app();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let peliculas = [];
+let idPelicula = 1;
+
+app.post('/peliculas', (req, res) => {
+    const nuevaPelicula = { id: idPelicula++, ...req.body };
+    peliculas.push(nuevaPelicula);
+    res.json({ mensaje: 'Película agregada con éxito', pelicula: nuevaPelicula });
+});
+
+app.post('/peliculas/:id', (req, res) => {
+    const idPelicula = parseInt(req.params.id);
+    const datosActualizados = req.body;
 
 
-server.put("/:valor", (req,res) =>{
+    const pelicula = peliculas.find(p => p.id === idPelicula);
+    if (pelicula) {
+        pelicula.titulo = datosActualizados.titulo || pelicula.titulo;
+        pelicula.director = datosActualizados.director || pelicula.director;
+        res.json({ mensaje: 'Información de la película actualizada con éxito', pelicula });
+    } else {
+        res.status(404).json({ mensaje: 'Película no encontrada' });
+    }
+});
 
-})
+const puerto = 3000;
+app.listen(puerto, () => {
+    console.log(`La API de películas está escuchando en http://localhost:${puerto}`);
+});
 
-server.post("/:valor:index", (req,res) =>{
-
-})
-
-server.delete("/:index", (req,res) =>{
-
-})
-
-server.get("/:index",(req,res) =>{
-
-})
-server.listen(3001,() => {
-    console.log("Server is running")
-    })
